6b45bb14bbbfb594908603d95ff991f2278ed9a5
kean <grebenyuk.alexander@gmail.com>
2014-02-28 00:07:14 +0400

    DFJPEGTurboTests

--- DFJPEGTurboTests/DFJPEGTurboTests.m
+++ DFJPEGTurboTests/DFJPEGTurboTests.m
@@ -6,6 +6,11 @@
 //  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
 //
 
+#import "DFBenchmark.h"
+#import "DFImageProcessing.h"
+#import "DFJPEGTurbo.h"
+#import "SDWebImageDecoder.h"
+#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 @interface DFJPEGTurboTests : XCTestCase
@@ -14,21 +19,34 @@
 
 @implementation DFJPEGTurboTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testPerformance {
+    NSString * filePath = [[NSBundle bundleForClass:[self class] ] pathForResource:@"sample-01" ofType:@"jpeg"];
+    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+    NSArray *images = @[ [DFImageProcessing imageWithImage:image scaledToSize:DFSizeScaled(image.size, 0.5f)], image, [DFImageProcessing imageWithImage:image scaledToSize:DFSizeScaled(image.size, 2.f)]];
+    for (UIImage *image in images) {
+        [self _benchmarkWithImage:image compressionQuality:0.33f];
+        [self _benchmarkWithImage:image compressionQuality:0.66f];
+        [self _benchmarkWithImage:image compressionQuality:1.f];
+    }
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)_benchmarkWithImage:(UIImage *)image compressionQuality:(CGFloat)compressionQuality {
+    NSLog(@"---------------------------------------------------");
+    NSLog(@"Benchmarking jpeg decompression with image size: (%f, %f), compression quality: (%f)", image.size.width, image.size.height, compressionQuality);
+    NSData *data = UIImageJPEGRepresentation(image, compressionQuality);
+    NSLog(@"Benchmark: SDWebImageDecoder");
+    dwarf_benchmark(YES, ^{
+        @autoreleasepool {
+            UIImage *image = [UIImage imageWithData:data];
+            __attribute__((unused)) UIImage *decodedImage = [UIImage decodedImageWithImage:image];
+        }
+    });
+    NSLog(@"Benchmark: DFJPEGTurbo");
+    dwarf_benchmark(YES, ^{
+        @autoreleasepool {
+            __attribute__((unused)) UIImage *decodedImage = [DFJPEGTurbo jpegImageWithData:data orientation:UIImageOrientationDown];
+        }
+    });
 }
 
 @end
